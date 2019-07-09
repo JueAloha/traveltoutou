85ea3cdc6b508475d94a1f3290c8d8110fad3914
Jue_Aloha <julie.vinet@free.fr>
2019-07-09 10:07:24 +0200

    [FRONT - Login] Add formulaire for inscription

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,11 @@
-//import React from 'react';
-//import logo from './logo.svg';
 import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
 import Home from './view/home'
 import Login from './view/login'
 import Account from './view/account'
 import Ad from './view/ad'
 import Listing from './view/listing'
-
-
 
 class App extends Component {
   render() {
