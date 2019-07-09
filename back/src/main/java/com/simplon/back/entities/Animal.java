ba4b182ff932b7851ec10bc2e5ed23684f27b740
pierrepoulouin <pierrepoulouin@gmail.com>
2019-07-09 14:12:51 +0200

    Security part

--- back/src/main/java/com/simplon/back/entities/Animal.java
+++ back/src/main/java/com/simplon/back/entities/Animal.java
@@ -4,12 +4,17 @@ import lombok.Data;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @Entity
 public class Animal {
 
-    //TODO Enum species
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private long id;
     @Column(length = 100, nullable = false)
     private String species;
     @Column(length = 100, nullable = false)
--- back/src/main/java/com/simplon/back/repositories/UserJpaRepository.java
+++ back/src/main/java/com/simplon/back/repositories/UserJpaRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserJpaRepository extends JpaRepository<User, Long> {
 
-
+    User findByUserAccountMail( String mail);
 }
