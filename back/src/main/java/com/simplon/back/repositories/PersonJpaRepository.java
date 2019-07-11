162d6497403c00f5c21399ca64701dfd9644a95d
pierrepoulouin <pierrepoulouin@gmail.com>
2019-07-11 14:31:07 +0200

    [BACK] CRUD done

--- back/src/main/java/com/simplon/back/repositories/PersonJpaRepository.java
+++ back/src/main/java/com/simplon/back/repositories/PersonJpaRepository.java
@@ -3,8 +3,11 @@ package com.simplon.back.repositories;
 import com.simplon.back.entities.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PersonJpaRepository extends JpaRepository<Person, Long> {
 
     Person findByUserAccountUsername(String username);
+
 
 }
--- back/src/main/java/com/simplon/back/services/UserService.java
+++ back/src/main/java/com/simplon/back/services/UserService.java
@@ -1,11 +1,18 @@
 package com.simplon.back.services;
 
 import com.simplon.back.dtos.UserCreateDto;
+import com.simplon.back.entities.Person;
+
+import java.util.List;
 
 public interface UserService {
 
     void create(UserCreateDto user);
 
     void delete(Long id);
+
+    Person update(UserCreateDto user, Long id);
+
+    List getAllUsers();
 
 }
